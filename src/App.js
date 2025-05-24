24c0c90f75fd7f62542fb6b8d75bb8aa0394fac6
Yogendra Neeladri <115712703+YogendraNeeladri@users.noreply.github.com>
2025-05-24 09:07:41 +0530

    App.js

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import store from './store';
+import Calendar from './components/Calendar';
+
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#1976d2',
+    },
+    secondary: {
+      main: '#dc004e',
+    },
+  },
+});
+
+function App() {
+  return (
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <CssBaseline />
+          <Calendar />
+        </LocalizationProvider>
+      </ThemeProvider>
+    </Provider>
+  );
+}
+
+export default App; 
